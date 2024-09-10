07144b0e52d978d44bd4850fe5637af7b17a8921
Manik Maity <manikmaity156@gmail.com>
2024-09-10 17:24:26 +0530

    Job board completed

--- Job-Board/src/components/JobBoard.tsx
+++ Job-Board/src/components/JobBoard.tsx
@@ -2,31 +2,50 @@ import { useEffect, useState } from "react";
 import { JobObj } from "../types";
 import JobNewsCard from "./JobNewsCard";
 
-const exampleData : JobObj = {
-    "by": "jamilbk",
-    "id": 35908337,
-    "score": 1,
-    "time": 1683838872,
-    "title": "Firezone (YC W22) is hiring Elixir and Rust engineers",
-    "type": "job",
-    "url": "https://www.ycombinator.com/companies/firezone/jobs"
-  }
 
   const jobIdsUrl : string = "https://hacker-news.firebaseio.com/v0/jobstories.json";
   const jobNewsDataUrl : string = "https://hacker-news.firebaseio.com/v0/item/"; // + 35908337.json
 
 const JobBoard = () => {
 
-    const [JobsNews, setJobsNews] = useState([exampleData, exampleData]);
-    const [jobIds, setJobIds] = useState([]);
-    const [sixJobNews, setSixJobNews] = useState([]);
+    const [JobsNews, setJobsNews] = useState<JobObj[]>([]);
+    const [jobIds, setJobIds] = useState<number[]>([]);
     const [isFetching, setIsFetching] = useState(false);
+    const [currentItem, setCurrentItem] = useState(0);
+    const perPageItem = 6;
 
 
-    useEffect(() => {
-        
+    function handleFetchPage(){
+      fetchingItems(currentItem);
+    }
 
+    async function fetchingItems (currentItem : number){
+      setIsFetching(true);
+      let jobList = jobIds;
+      if (jobList.length <= 0){
+        const jobIdsDataJson = await fetch(jobIdsUrl);
+        jobList = await jobIdsDataJson.json();
+        setJobIds(jobList);
+      }
+      
+      const jobIdsForPage = jobList.slice(currentItem, currentItem+perPageItem);
+      setCurrentItem(currentItem+perPageItem);
+      const jobDatasForPage = await Promise.all(
+        jobIdsForPage.map((id : number) => fetch(`${jobNewsDataUrl}${id}.json`).then(res => res.json()))
+      );
+      setJobsNews([...JobsNews, ...jobDatasForPage]);
+      setIsFetching(false);
+      
+    }
+
+
+    useEffect(()=> {
+      if (currentItem == 0) {
+        fetchingItems(currentItem);
+      }
     }, [])
+
+
 
 
   return (
@@ -40,7 +59,7 @@ const JobBoard = () => {
         }
       </div>
 
-      <button className="load-btn">{isFetching ? "Loading.." : "Fetch More Jobs"}</button>
+      <button disabled={currentItem+perPageItem > jobIds.length} onClick={handleFetchPage} className="load-btn">{isFetching ? "Loading.." : "Fetch More Jobs"}</button>
 
     </div>
   )
